9248564b1756380f79bfead44c5f84a59d882115
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-03 02:49:20 +0000

    Added Size Class

---
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/Size.java
@@ -0,0 +1,19 @@
+package com.ldtsfeup2526.bobTheDestructor.model;
+
+public class Size {
+    private int x;
+    private int y;
+
+    public Size(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
