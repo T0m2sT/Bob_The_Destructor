c36bb121a7952f4a0790a87b38aa9f7b5d8984f8
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-15 22:43:03 +0000

    Added background effect

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/view/game/SceneViewer.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/view/game/SceneViewer.java
@@ -3,6 +3,7 @@ package com.ldtsfeup2526.bobTheDestructor.view.game;
 import com.ldtsfeup2526.bobTheDestructor.gui.GUI;
 import com.ldtsfeup2526.bobTheDestructor.model.game.scene.Scene;
 import com.ldtsfeup2526.bobTheDestructor.model.spatials.Position;
+import com.ldtsfeup2526.bobTheDestructor.view.Animation;
 import com.ldtsfeup2526.bobTheDestructor.view.ElementViewer;
 import com.ldtsfeup2526.bobTheDestructor.view.Sprite;
 import com.ldtsfeup2526.bobTheDestructor.view.SpriteLoader;
@@ -14,10 +15,19 @@ import java.util.Map;
 
 public class SceneViewer implements ElementViewer<Scene> {
     private final Map<String, Sprite[]> spriteMap = new HashMap<>();
+    private final Animation backgroundAnim;
     private final SpriteLoader spriteLoader;
 
-    public SceneViewer(SpriteLoader spriteLoader) {
+    public SceneViewer(SpriteLoader spriteLoader) throws IOException {
         this.spriteLoader = spriteLoader;
+
+        backgroundAnim = new Animation(new Sprite[] {
+                spriteLoader.get("caves/background/bg1.png"),
+                spriteLoader.get("caves/background/bg2.png"),
+                spriteLoader.get("caves/background/bg3.png")},
+                0.125f,
+                true
+        );
     }
 
     public void retrieveCaves(List<String> cavesPath) throws IOException {
@@ -32,6 +42,10 @@ public class SceneViewer implements ElementViewer<Scene> {
     public void draw(Scene model, GUI gui, double deltaTime) {
         Sprite structureSprite = spriteMap.get(model.getCaveFilePath())[0];
         Sprite detailsSprite = spriteMap.get(model.getCaveFilePath())[1];
+
+        backgroundAnim.update(deltaTime);
+        backgroundAnim.getSprites()[backgroundAnim.getFrame()].draw(new Position(0, 0), gui);
+
         structureSprite.draw(new Position(0, 0), gui);
         detailsSprite.draw(new Position(0, 0), gui);
     }
