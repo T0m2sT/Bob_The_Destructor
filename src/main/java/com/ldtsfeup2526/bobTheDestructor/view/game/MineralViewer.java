dc99c703a62d487c40fd7bdbb1483f2d90254700
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-16 15:12:20 +0000

    Gave Each model a animation

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/view/game/MineralViewer.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/view/game/MineralViewer.java
@@ -17,6 +17,7 @@ import java.util.Random;
 public class MineralViewer implements ElementViewer<MineralModel> {
     private final Map<MineralType, Animation[]> spriteMap;
     private final Random random = new Random();
+    private final Map<MineralModel, Animation[]> animationPerModel = new HashMap<>();
 
     public MineralViewer(SpriteLoader spriteLoader) throws IOException {
         spriteMap = new HashMap<>();
@@ -25,9 +26,8 @@ public class MineralViewer implements ElementViewer<MineralModel> {
                 spriteLoader.get("sprites/gems/gem1.png"),
                 spriteLoader.get("sprites/gems/gem2.png"),
                 spriteLoader.get("sprites/gems/gem3.png"),
-                spriteLoader.get("sprites/gems/gem4.png"),
-                spriteLoader.get("sprites/gems/gem5.png")},
-                3.5,
+                spriteLoader.get("sprites/gems/gem4.png")},
+                0.25,
                 true
         );
 
@@ -36,7 +36,7 @@ public class MineralViewer implements ElementViewer<MineralModel> {
                 spriteLoader.get("sprites/gems/cracked/pink_crack2.png"),
                 spriteLoader.get("sprites/gems/cracked/pink_crack3.png"),
                 spriteLoader.get("sprites/gems/cracked/pink_crack4.png")},
-                1,
+                0.25,
                 false
         );
 
@@ -46,9 +46,8 @@ public class MineralViewer implements ElementViewer<MineralModel> {
                 spriteLoader.get("sprites/gems/gem7.png"),
                 spriteLoader.get("sprites/gems/gem8.png"),
                 spriteLoader.get("sprites/gems/gem9.png"),
-                spriteLoader.get("sprites/gems/gem10.png"),
-                spriteLoader.get("sprites/gems/gem11.png")},
-                3.5,
+                spriteLoader.get("sprites/gems/gem10.png")},
+                0.25,
                 true
         );
 
@@ -57,7 +56,7 @@ public class MineralViewer implements ElementViewer<MineralModel> {
                 spriteLoader.get("sprites/gems/cracked/yellow_crack2.png"),
                 spriteLoader.get("sprites/gems/cracked/yellow_crack3.png"),
                 spriteLoader.get("sprites/gems/cracked/yellow_crack4.png")},
-                1,
+                0.25,
                 false
         );
 
@@ -67,9 +66,8 @@ public class MineralViewer implements ElementViewer<MineralModel> {
                 spriteLoader.get("sprites/gems/gem13.png"),
                 spriteLoader.get("sprites/gems/gem14.png"),
                 spriteLoader.get("sprites/gems/gem15.png"),
-                spriteLoader.get("sprites/gems/gem16.png"),
-                spriteLoader.get("sprites/gems/gem17.png")},
-                3.5,
+                spriteLoader.get("sprites/gems/gem16.png")},
+                0.25,
                 true
         );
 
@@ -78,7 +76,7 @@ public class MineralViewer implements ElementViewer<MineralModel> {
                 spriteLoader.get("sprites/gems/cracked/blue_crack2.png"),
                 spriteLoader.get("sprites/gems/cracked/blue_crack3.png"),
                 spriteLoader.get("sprites/gems/cracked/blue_crack4.png")},
-                1,
+                0.25,
                 false
         );
 
@@ -87,7 +85,7 @@ public class MineralViewer implements ElementViewer<MineralModel> {
 
     @Override
     public void draw(MineralModel model, GUI gui, double deltaTime) {
-        Animation anim = spriteMap.get(model.getType())[0];
+        Animation anim = createAnimForSingleModel(model)[0];
         anim.update(deltaTime);
         Sprite sprite = anim.getSprites()[anim.getFrame()];
         switch (model.getDirection()) {
@@ -104,5 +102,14 @@ public class MineralViewer implements ElementViewer<MineralModel> {
                 sprite.drawRotRight(model.getPosition(), gui);
                 break;
         }
+    }
+
+    private Animation[] createAnimForSingleModel(MineralModel model) {
+        if (!animationPerModel.containsKey(model)) {
+            Animation[] anims = spriteMap.get(model.getType());
+            animationPerModel.put(model, new Animation[] {anims[0].copy(), anims[1].copy()});
+        }
+
+        return animationPerModel.get(model);
     }
 }
