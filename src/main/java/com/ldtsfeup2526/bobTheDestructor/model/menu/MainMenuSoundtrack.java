e00534511fe2b638e5813a8f041fd0a93ce83372
alexis-ramoss <up202404977@edu.fe.up.pt>
2025-12-16 19:52:32 +0000

    Implemented NullSoundPlayer in case the soundtrack fails to load and replaced dependent code

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/model/menu/MainMenuSoundtrack.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/menu/MainMenuSoundtrack.java
@@ -2,15 +2,29 @@ package com.ldtsfeup2526.bobTheDestructor.model.menu;
 
 import javax.sound.sampled.*;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 
 public class MainMenuSoundtrack {
     private final AudioInputStream audioInput;
     private final Clip soundtrackClip;
 
-    public MainMenuSoundtrack() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        audioInput = AudioSystem.getAudioInputStream(Objects.requireNonNull(getClass().getClassLoader().getResource("sounds/mainMenuSoundtrack.wav")));
-        soundtrackClip = AudioSystem.getClip();
+    public MainMenuSoundtrack() {
+        AudioInputStream input = null;
+        Clip clip = null;
+        try {
+            URL url = getClass().getClassLoader().getResource("sounds/mainMenuSoundtrack.wav");
+            if (url != null) {
+                input = AudioSystem.getAudioInputStream(url);
+                clip = AudioSystem.getClip();
+            } else {
+                // No soundtrack
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.audioInput = input;
+        this.soundtrackClip = clip;
     }
 
     public AudioInputStream getAudioInput() {
