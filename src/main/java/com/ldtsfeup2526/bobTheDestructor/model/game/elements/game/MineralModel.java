699049c6738a09c9854741d0bce9352dca2dbce4
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-16 04:40:09 +0000

    Changed MineralTypes and MineralModel

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/elements/game/MineralModel.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/elements/game/MineralModel.java
@@ -4,32 +4,15 @@ import com.ldtsfeup2526.bobTheDestructor.model.game.elements.ElementModel;
 import com.ldtsfeup2526.bobTheDestructor.model.spatials.Position;
 
 public class MineralModel extends ElementModel {
-    public enum Type {COAL, IRON, GOLD, DIAMOND}
 
-    private int health;
+    private final MineralType type;
 
-    private final Type type;
-
-    public MineralModel(Position position, Type type) {
+    public MineralModel(Position position, MineralType type) {
         super(position);
         this.type = type;
-        switch (type) {
-            case DIAMOND: health = 20; break;
-            case GOLD: health = 10; break;
-            case IRON: health = 3; break;
-            default: health = 1; break;
-        }
     }
 
-    public Type getType() {
+    public MineralType getType() {
         return type;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
     }
 }
---
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/elements/game/MineralType.java
@@ -0,0 +1,7 @@
+package com.ldtsfeup2526.bobTheDestructor.model.game.elements.game;
+
+public enum MineralType {
+    PINK,
+    BLUE,
+    YELLOW
+}
