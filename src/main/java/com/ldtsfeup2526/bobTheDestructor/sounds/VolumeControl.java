b0ec6eddda9f5a7d4b72e407ee19deb5be58e027
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-26 22:40:03 +0000

    Fixed Volume Control Interface

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/sounds/VolumeControl.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/sounds/VolumeControl.java
@@ -1,6 +1,10 @@
 package com.ldtsfeup2526.bobTheDestructor.sounds;
 
 public interface VolumeControl {
-    void setVolume(float volume);
-    float getVolume();
+    void setMasterVolume(float volume);
+    void setMusicVolume(float volume);
+    void setSFXVolume(float volume);
+    float getMasterVolume();
+    float getMusicVolume();
+    float getSFXVolume();
 }
