bef1e137b4047e40f718f55472d33b67ab492f60
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-28 23:01:30 +0000

    Title Viewer Implemented to the game settings

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/view/screens/SettingsMenuViewer.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/view/screens/SettingsMenuViewer.java
@@ -3,9 +3,11 @@ package com.ldtsfeup2526.bobTheDestructor.view.screens;
 import com.googlecode.lanterna.TextColor;
 import com.ldtsfeup2526.bobTheDestructor.gui.GUI;
 import com.ldtsfeup2526.bobTheDestructor.model.menu.SettingsMenu;
+import com.ldtsfeup2526.bobTheDestructor.model.spatials.Position;
 import com.ldtsfeup2526.bobTheDestructor.view.ViewerProvider;
 import com.ldtsfeup2526.bobTheDestructor.view.menu.ButtonViewer;
 import com.ldtsfeup2526.bobTheDestructor.view.menu.SliderViewer;
+import com.ldtsfeup2526.bobTheDestructor.view.menu.TitleViewer;
 import com.ldtsfeup2526.bobTheDestructor.view.menu.WallpaperViewer;
 
 import java.io.IOException;
@@ -13,11 +15,13 @@ import java.io.IOException;
 public class SettingsMenuViewer extends ScreenViewer<SettingsMenu> {
     private final SliderViewer sliderViewer;
     private final WallpaperViewer wallpaperViewer;
+    private final TitleViewer titleViewer;
 
     public SettingsMenuViewer(SettingsMenu model, ViewerProvider viewerProvider) {
         super(model);
         this.sliderViewer = viewerProvider.getSliderViewer();
         this.wallpaperViewer = viewerProvider.getWallpaperViewer();
+        this.titleViewer = viewerProvider.getTitleViewer();
     }
 
     public void draw(GUI gui, double deltaTime) throws IOException {
@@ -25,6 +29,8 @@ public class SettingsMenuViewer extends ScreenViewer<SettingsMenu> {
         gui.drawBackground(new TextColor.RGB(57, 53, 74));
         wallpaperViewer.draw(gui);
         drawElements(gui, getModel().getWidgets(), sliderViewer, deltaTime);
+        titleViewer.draw(new Position(83, 10), "settings", gui);
+
         gui.refresh();
     }
 }
