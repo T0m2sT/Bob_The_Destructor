c1407ef92ed3f3513930499d3b027cf8ed1624f5
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-28 21:59:50 +0000

    Added More Sliders to Config

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/model/menu/SettingsMenu.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/menu/SettingsMenu.java
@@ -4,13 +4,17 @@ import com.ldtsfeup2526.bobTheDestructor.Game;
 import com.ldtsfeup2526.bobTheDestructor.model.spatials.Position;
 
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class SettingsMenu extends Menu {
     @Override
     protected List<Widget> createWidgets() {
-        Widget volume = new Widget(WidgetType.MASTER_VOLUME, WidgetState.SELECTED, new Position(Game.resolution.width()/2, (Game.resolution.height()/2)));
-        return Collections.singletonList(volume);
+        Widget masterVolume = new Widget(WidgetType.MASTER_VOLUME, WidgetState.UNSELECTED, new Position(Game.resolution.width()/2, (Game.resolution.height()/2 - 20)));
+        Widget musicVolume = new Widget(WidgetType.MUSIC_VOLUME, WidgetState.UNSELECTED, new Position(Game.resolution.width()/2, (Game.resolution.height()/2)));
+        Widget sfxVolume = new Widget(WidgetType.SFX_VOLUME, WidgetState.UNSELECTED, new Position(Game.resolution.width()/2, (Game.resolution.height()/2 + 20)));
+
+        return Arrays.asList(masterVolume, musicVolume, sfxVolume);
     }
 }
