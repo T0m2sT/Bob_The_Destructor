01644970a093efada11dec90a5845ee556afad0f
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-29 17:01:38 +0000

    Added stats

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/model/stats/Stats.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/stats/Stats.java
@@ -3,7 +3,25 @@ package com.ldtsfeup2526.bobTheDestructor.model.stats;
 import com.ldtsfeup2526.bobTheDestructor.model.game.scene.SceneManager;
 
 public class Stats {
-    public Stats(SceneManager sceneManager) {
+    private final int mineralsCollected;
+    private final int seconds;
+    private final int minutes;
 
+    public Stats(SceneManager sceneManager) {
+        this.mineralsCollected = sceneManager.getTotalMineralsCollected();
+        this.seconds = (int) ((sceneManager.getTimePassed() / 1000) % 60);
+        this.minutes = (int) ((sceneManager.getTimePassed() / 1000) / 60);
+    }
+
+    public int getMineralsCollected() {
+        return mineralsCollected;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public int getMinutes() {
+        return minutes;
     }
 }
