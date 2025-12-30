4488f978e064f9eba118d004981234807beb95ab
T0m2sT <pedrotomasteixeira@gmail.com>
2025-12-30 03:24:29 +0000

    Refactored `SliderViewerTest` to remove redundant comments, simplifying and clarifying widget draw logic validation.

--- src/test/java/com/ldtsfeup2526/bobTheDestructor/view/menu/SliderViewerTest.java
+++ src/test/java/com/ldtsfeup2526/bobTheDestructor/view/menu/SliderViewerTest.java
@@ -43,16 +43,6 @@ public class SliderViewerTest {
         
         SliderViewer localViewer = new SliderViewer(localLoader);
         
-        // Master Volume (13 chars: "MASTER VOLUME")
-        // halfWidth = 13 * 5 / 2 = 32
-        // startTextPos = (100 - 32, 50) = (68, 50)
-        // leftArrowPos = (100 - 32 - 7, 50 + 2) = (61, 52)
-        // rightArrowPos = (100 + 32 + 2, 50 + 2) = (134, 52)
-        
-        // Value 50 (2 chars: "50")
-        // halfValueTextWidth = 2 * 5 / 2 = 5
-        // startValueTextPos = (100 - 5, 50 + 7) = (95, 57)
-        
         GameSettings.getInstance().setMasterVolume(0.5f);
         Widget wMaster = new Widget(WidgetType.MASTER_VOLUME, WidgetState.SELECTED, widgetPos);
         localViewer.draw(wMaster, gui, 0.1);
@@ -77,21 +67,16 @@ public class SliderViewerTest {
         
         SliderViewer localViewer = new SliderViewer(localLoader);
 
-        // Music Volume 0.75 -> 75
         GameSettings.getInstance().setMusicVolume(0.75f);
         Widget wMusic = new Widget(WidgetType.MUSIC_VOLUME, WidgetState.UNSELECTED, widgetPos);
         localViewer.draw(wMusic, gui, 0.1);
-        // Verify rounding: 75 has 2 digits
-        // Music Volume (12 chars: "MUSIC VOLUME")
-        // halfWidth = 12 * 5 / 2 = 30
+
         verify(letterSprite, atLeastOnce()).draw(argThat(p -> p.getX() == 100 - 30 && p.getY() == 50), eq(gui));
 
-        // SFX Volume 0.123 -> 12
         GameSettings.getInstance().setSfxVolume(0.123f);
         Widget wSfx = new Widget(WidgetType.SFX_VOLUME, WidgetState.UNSELECTED, widgetPos);
         localViewer.draw(wSfx, gui, 0.1);
-        // SFX Volume (10 chars: "SFX VOLUME")
-        // halfWidth = 10 * 5 / 2 = 25
+
         verify(letterSprite, atLeastOnce()).draw(argThat(p -> p.getX() == 100 - 25 && p.getY() == 50), eq(gui));
     }
     @Test
@@ -109,11 +94,7 @@ public class SliderViewerTest {
 
         Widget wDefault = new Widget(WidgetType.PLAY, WidgetState.UNSELECTED, widgetPos);
         localViewer.draw(wDefault, gui, 0.1);
-        
-        // value should be -1 -> rounded to -100
-        // "-100" has 4 characters
-        // halfValueTextWidth = 4 * 5 / 2 = 10
-        // startValueTextPos = 100 - 10 = 90
+
         verify(letterSprite, atLeastOnce()).draw(argThat(p -> p.getX() == 90 && p.getY() == 57), eq(gui));
     }
 }
