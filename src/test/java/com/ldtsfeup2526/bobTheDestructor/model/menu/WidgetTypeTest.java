e24b4aebc0f58efefb8231804d5a48927f82b78b
T0m2sT <pedrotomasteixeira@gmail.com>
2025-12-30 02:56:51 +0000

    Added unit test for `WidgetType` to verify enum values and integrity.

---
+++ src/test/java/com/ldtsfeup2526/bobTheDestructor/model/menu/WidgetTypeTest.java
@@ -0,0 +1,18 @@
+package com.ldtsfeup2526.bobTheDestructor.model.menu;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WidgetTypeTest {
+    @Test
+    void testEnumValues() {
+        assertEquals(7, WidgetType.values().length);
+        assertNotNull(WidgetType.valueOf("PLAY"));
+        assertNotNull(WidgetType.valueOf("CONFIG"));
+        assertNotNull(WidgetType.valueOf("CREDITS"));
+        assertNotNull(WidgetType.valueOf("EXIT"));
+        assertNotNull(WidgetType.valueOf("MASTER_VOLUME"));
+        assertNotNull(WidgetType.valueOf("MUSIC_VOLUME"));
+        assertNotNull(WidgetType.valueOf("SFX_VOLUME"));
+    }
+}
